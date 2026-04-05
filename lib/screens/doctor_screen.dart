dcc1b1c50a79f3f8dd08bb5fdfc4b02bea53f0f9
Muhammad Hassan <immaharhasaan@gmail.com>
2026-04-05 11:15:49 +0500

    update

---
+++ lib/screens/doctor_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'records_screen.dart';
+
+class DoctorScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Doctor Panel")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Text("Welcome Doctor 👨‍⚕️",
+                style: TextStyle(fontSize: 22)),
+
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (_) => RecordsScreen()));
+              },
+              child: Text("View Medical Records"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
