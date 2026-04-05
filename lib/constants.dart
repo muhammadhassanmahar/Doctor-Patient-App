2204e24890aa59d1414ca5910239a8b88830cb0b
Muhammad Hassan <immaharhasaan@gmail.com>
2026-04-05 11:24:20 +0500

    update

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+// ⚠️ Android emulator -> 10.0.2.2
+// ⚠️ Real device -> PC IP address
+const String BASE_URL = "http://10.0.2.2:8000";
+
+// API Endpoints
+const String LOGIN_URL = "/login";
+const String REGISTER_URL = "/register";
+const String RECORDS_URL = "/medical-records";
