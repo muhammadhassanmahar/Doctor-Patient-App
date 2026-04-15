2b281f10438adc834b08fc70d3f8dc5ad98876d6
Muhammad Hassan <immaharhasaan@gmail.com>
2026-04-15 17:21:07 +0500

    update

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import '../utils/constants.dart';
+
+class CustomButton extends StatefulWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  State<CustomButton> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  bool pressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) => setState(() => pressed = true),
+      onTapUp: (_) {
+        setState(() => pressed = false);
+        widget.onPressed();
+      },
+      onTapCancel: () => setState(() => pressed = false),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 150),
+        transform: Matrix4.identity()..scale(pressed ? 0.95 : 1.0),
+        padding: const EdgeInsets.symmetric(vertical: 14),
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(
+            colors: [AppConstants.primaryColor, AppConstants.secondaryColor],
+          ),
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            if (!pressed)
+              const BoxShadow(
+                color: Colors.black26,
+                blurRadius: 8,
+                offset: Offset(0, 4),
+              ),
+          ],
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          widget.text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
