f6026897a677fdc482c8653e6480067f76d878b4
Muhammad Hassan <immaharhasaan@gmail.com>
2026-04-15 17:20:12 +0500

    update

---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const String baseUrl = "http://10.0.2.2:8000";
+
+  // Colors (theme)
+  static const Color primaryColor = Colors.blue;
+  static const Color secondaryColor = Colors.purple;
+
+  // App name
+  static const String appName = "Doctor App";
+}
